d26296b7c7eb9f28fb73cac9b060b13f694bfa28
Joe Attardi <jattardi@salesforce.com>
2019-03-25 16:10:14 -0400

    add mime type handling

--- src/handlers/file.js
+++ src/handlers/file.js
@@ -3,5 +3,33 @@ const fs = require('fs-extra');
 exports.sendFile = async function sendFile(resourcePath, request, response) {
   const fileContents = await fs.readFile(resourcePath);
 
+  const mimeType = mimeTypes[resourcePath.split('.').slice(-1)];
+
+  if (mimeType) {
+    response.setHeader('Content-Type', mimeType);
+  }
   response.end(fileContents);
 };
+
+const mimeTypes = {
+  'css': 'text/css',
+  'csv': 'text/csv',
+  'gif': 'image/gif',
+  'html': 'text/html',
+  'htm': 'text/html',
+  'ico': 'image/vnd.microsoft.icon',
+  'jpg': 'image/jpeg',
+  'jpeg': 'image/jpeg',
+  'js': 'text/javascript',
+  'json': 'application/json',
+  'mjs': 'text/javascript',
+  'pdf': 'application/pdf',
+  'png': 'image/png',
+  'rtf': 'application/rtf',
+  'svg': 'image/svg+xml',
+  'svgz': 'image/svg+xml',
+  'txt': 'text/plain',
+  'xhtml': 'application/xhtml+xml',
+  'xml': 'text/xml',
+  'zip': 'application/zip'
+}
